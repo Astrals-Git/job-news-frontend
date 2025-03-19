d1b21c13a475227efd8ca4cb4d6326096876ddba
Astrals-Git <khandelia.anant@hotmail.com>
2025-03-19 07:55:40 +0000

    Added backend API URL in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_BACKEND_API: "https://job-news-backend.onrender.com",
+  },
 };
 
 export default nextConfig;
+
